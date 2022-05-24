cb25d8cbc83a745b54b907e88a3f6c94e4e9a9b3
“GB” <ext_gnbascur@falabella.cl>
2022-05-24 19:51:36 -0400

    webserver

---
+++ app.js
@@ -0,0 +1,39 @@
+require ('dotenv').config();
+const express = require('express')
+const app = express()
+const port = process.env.PORT;
+const hbs = require('hbs');
+
+//Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+//servir contenido estatico
+app.use( express.static('public') )
+
+app.get('/',  (req, res) => {
+    res.render('home', {
+        nombre: 'Gaspar Bascur',
+        titutlo: 'test pagina web'
+    });
+});
+
+app.get('/generic',  (req, res) => {
+    res.render('generic', {
+        nombre: 'Gaspar Bascur',
+        titutlo: 'test pagina web'
+    });
+});
+
+app.get('/elements',  (req, res) => {
+    res.render('elements', {
+        nombre: 'Gaspar Bascur',
+        titutlo: 'test pagina web'
+    });
+});
+
+
+
+app.listen(port, () =>{
+    console.log(`El ejemplo de la app esta usando el puerto en http://localhost:${port}`);
+})
